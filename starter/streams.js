7c5b8a4099426858a6426ed32270abe9b09511cd
rodisho <rafiodisho@gmail.com>
2020-09-09 01:25:35 -0400

    Implement stream to process data in file - solution 2

--- starter/streams.js
+++ starter/streams.js
@@ -2,10 +2,27 @@ const fs = require("fs");
 const server = require("http").createServer();
 
 server.on("request", (req, res) => {
-  // Solution 1
-  fs.readFile("test-file.txt", (err, data) => {
-    if (err) console.log("ERROR ", err);
-    res.end(data);
+  // Solution 1 -> problem is that node have to load all file in order to process it!
+  // fs.readFile("test-file.txt", (err, data) => {
+  //   if (err) console.log("ERROR ", err);
+  //   res.end(data);
+
+  // });
+
+  // Solution 2: Streams
+  const readable = fs.createReadStream("test-file.txt");
+  readable.on("data", (chunck) => {
+    res.write(chunck);
+  });
+
+  readable.on("end", () => {
+    res.end();
+  });
+
+  readable.on("error", (err) => {
+    console.log("ERROR ", err);
+    res.statusCode = 500;
+    res.end("File not found!");
   });
 });
 
