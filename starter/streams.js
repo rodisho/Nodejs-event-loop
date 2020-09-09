a28aea2d01efdfabf493206ea12404a47a71b278
rodisho <rafiodisho@gmail.com>
2020-09-09 01:28:51 -0400

    Implement pipe for streaming data

--- starter/streams.js
+++ starter/streams.js
@@ -10,20 +10,25 @@ server.on("request", (req, res) => {
   // });
 
   // Solution 2: Streams
+  // const readable = fs.createReadStream("test-file.txt");
+  // readable.on("data", (chunck) => {
+  //   res.write(chunck);
+  // });
+
+  // readable.on("end", () => {
+  //   res.end();
+  // });
+
+  // readable.on("error", (err) => {
+  //   console.log("ERROR ", err);
+  //   res.statusCode = 500;
+  //   res.end("File not found!");
+  // });
+
+  // Solution 3
   const readable = fs.createReadStream("test-file.txt");
-  readable.on("data", (chunck) => {
-    res.write(chunck);
-  });
-
-  readable.on("end", () => {
-    res.end();
-  });
-
-  readable.on("error", (err) => {
-    console.log("ERROR ", err);
-    res.statusCode = 500;
-    res.end("File not found!");
-  });
+  readable.pipe(res);
+  // readableSource.pipe(writeableDest)
 });
 
 server.listen(8000, "127.0.0.1", () => {
