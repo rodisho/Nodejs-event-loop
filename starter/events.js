593379a5729ac9f4d49577a4bd10a5a9fe9e8911
rodisho <rafiodisho@gmail.com>
2020-09-08 23:55:43 -0400

    Added a new events file to demontrate emitting signales

---
+++ starter/events.js
@@ -0,0 +1,16 @@
+const EventEmitter = require("events");
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on("newSale", () => {
+  console.log("There was a new sale!");
+});
+
+myEmitter.on("newSale", () => {
+  console.log("Customer name: Rafi");
+});
+
+myEmitter.on("newSale", (stock) => {
+  console.log(`There are now ${stock} items left in stock.`);
+});
+myEmitter.emit("newSale", 9);
